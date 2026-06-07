78d71ccfa2d9c49ead81941a3f04c3ec836dd827
melang lama <melanglama00@gmail.com>
2026-06-07 14:34:35 +0545

    comiling and running

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+   //creation of method
+    void display() {
+        System.out.println("this is the example of method");
+    }
+
+    public static void main(String[] args) {
+
+        Student s = new Student();  // Create object
+
+        s.display();                // Call method
+    }
+}
