603b750b6fbaf039b20d215c5abb21bbcbd37063
melang lama <melanglama00@gmail.com>
2026-06-10 11:57:59 +0545

    the main file created and ATM example written

--- Main.java
+++ Main.java
@@ -1,18 +1,50 @@
-class Student {
-    String name = "Milan";
+abstract class ATM {
 
-    void display() {
-        System.out.println("Name: " + name);
+    // Abstract methods (what the ATM can do)
+    abstract void checkBalance();
+    abstract void deposit(double amount);
+    abstract void withdraw(double amount);
+}
+
+class BankATM extends ATM {
+
+    private double balance = 10000; // hidden data
+
+    @Override
+    void checkBalance() {
+        System.out.println("Current Balance: Rs. " + balance);
+    }
+
+    @Override
+    void deposit(double amount) {
+        balance += amount;
+        System.out.println("Rs. " + amount + " deposited successfully.");
+    }
+
+    @Override
+    void withdraw(double amount) {
+
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Please collect your cash: Rs. " + amount);
+        } else {
+            System.out.println("Insufficient balance.");
+        }
     }
 }
 
 public class Main {
+
     public static void main(String[] args) {
 
-        // Creating an object
-        Student s = new Student();
+        BankATM user = new BankATM();
 
-        // Calling method using the object
-        s.display();
+        user.checkBalance();
+
+        user.deposit(5000);
+
+        user.withdraw(3000);
+
+        user.checkBalance();
     }
 }
